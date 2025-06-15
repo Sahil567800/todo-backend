5e92a5fc37b1e8902db981e6eb9a3f4c365d28bb
Sahil567800 <sahilskp110@gmail.com>
2025-06-15 10:48:51 +0530

    hosted mongodb

--- conn.js
+++ conn.js
@@ -2,7 +2,7 @@ import mongoose from "mongoose";
 
 export const conn = async () => {
     try {
-        await mongoose.connect("mongodb://localhost:27017/todo")
+        await mongoose.connect("mongodb+srv://sahilskp110:sahil5678@todocluster.pecn54f.mongodb.net/?retryWrites=true&w=majority&appName=TodoCluster")
         console.log("database connected")
     } catch (error) {
         console.error(" database connection failed:", error.message);
